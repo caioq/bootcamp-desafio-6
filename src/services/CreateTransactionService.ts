d0556421adcbc42757d28934cf869277d2bbd489
Caio <caiogqueiroz@gmail.com>
2020-04-23 09:56:43 -0300

    criacao das tables com migrations

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -2,9 +2,22 @@
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({title value, type, category}: Request): Promise<Transaction> {
+    // busco categoria
+
+    // se nao existir categoria, salvo nova categoria
+
+    // salvo transacao
+
+    // return transaction
   }
 }
 
